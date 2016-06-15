7f9af87aab81bb7a851f7b7012c9705d76012a3f
mbhooshan <mani.bhooshan@gmail.com>
2016-06-15 01:19:14 -0700

    test

--- src/main/java/com/skry/ingestion/App.java
+++ src/main/java/com/skry/ingestion/App.java
@@ -1,5 +1,16 @@
 package com.skry.ingestion;
 
+import java.util.concurrent.ExecutionException;
+
+import org.bitcoinj.core.Block;
+import org.bitcoinj.core.BlockChain;
+import org.bitcoinj.core.Peer;
+import org.bitcoinj.core.StoredBlock;
+import org.bitcoinj.kits.WalletAppKit;
+import org.bitcoinj.params.MainNetParams;
+import org.bitcoinj.store.BlockStore;
+import org.bitcoinj.store.BlockStoreException;
+
 /**
  * Hello world!
  *
@@ -8,6 +19,33 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	try {
+			WalletAppKit kit = new WalletAppKit(MainNetParams.get(), new java.io.File("."), "test");
+			// Download headers
+			kit.startAsync();
+			BlockChain chain = kit.chain();
+			BlockStore bs = chain.getBlockStore();
+			Peer peer = kit.peerGroup().getDownloadPeer();
+			// Get last block
+			StoredBlock current = bs.getChainHead();
+			// Loop until you reach the genesis block
+			while(current.getHeight() > 1) {
+			    // Fully download blocks between 100 and 200
+			    if(100 <= current.getHeight() && current.getHeight() <= 200) {
+			        Block b = peer.getBlock(current.getHeader().getHash()).get();
+			        System.out.println(b);
+			    }
+			    current = current.getPrev(bs);
+			}
+		} catch (BlockStoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
--- src/main/java/com/skry/ingestion/FetchBlock.java
+++ src/main/java/com/skry/ingestion/FetchBlock.java
@@ -18,13 +18,18 @@
 package com.skry.ingestion;
 
 import org.bitcoinj.core.*;
+import org.bitcoinj.core.listeners.BlocksDownloadedEventListener;
+import org.bitcoinj.core.listeners.DownloadProgressTracker;
+import org.bitcoinj.net.discovery.DnsDiscovery;
 import org.bitcoinj.params.TestNet3Params;
 import org.bitcoinj.store.BlockStore;
 import org.bitcoinj.store.MemoryBlockStore;
+import org.bitcoinj.store.SPVBlockStore;
 import org.bitcoinj.utils.BriefLogFormatter;
 
 import com.google.common.net.InetAddresses;
 
+import java.io.File;
 import java.net.InetAddress;
 import java.util.concurrent.Future;
 
@@ -37,21 +42,58 @@ public class FetchBlock {
         System.out.println("Connecting to node");
         final NetworkParameters params = TestNet3Params.get();
 
+//        File chainFile = new File("seed.spvchain");
+//        SPVBlockStore chainStore = new SPVBlockStore(params, chainFile);
+//        BlockChain chain = new BlockChain(params, chainStore);
+//        PeerGroup peerGroup = new PeerGroup(params, chain);
+//        peerGroup.addPeerDiscovery(new DnsDiscovery(params));
+        
         BlockStore blockStore = new MemoryBlockStore(params);
         BlockChain chain = new BlockChain(params, blockStore);
         PeerGroup peerGroup = new PeerGroup(params, chain);
+        peerGroup.addPeerDiscovery(new DnsDiscovery(params));
+        
+       /* DownloadProgressTracker bListener = new DownloadProgressTracker() {
+            @Override
+            public void doneDownload() {
+                System.out.println("blockchain downloaded");
+            }
+        };*/
+        
         peerGroup.start();
+//        peerGroup.startBlockChainDownload(bListener);
+//        bListener.await();
+
+
+        // shutting down again
+//        peerGroup.stop();
+        
 //        PeerAddress addr = new PeerAddress(InetAddress.getLocalHost(), params.getPort());
-        PeerAddress addr = new PeerAddress(InetAddress.getByAddress("192.168.1.121", "8333".getBytes()));
-        peerGroup.addAddress(addr);
+//        PeerAddress addr = new PeerAddress(InetAddress.getByAddress("192.168.1.121", "8333".getBytes()));
+//        peerGroup.addAddress(addr);
         peerGroup.waitForPeers(1).get();
         Peer peer = peerGroup.getConnectedPeers().get(0);
-
-        Sha256Hash blockHash = Sha256Hash.wrap(args[0]);
+        Sha256Hash blockHash = Sha256Hash.wrap("0000000000001386495b6b32a3e4ef1c3a05389b896d3c87e3ff4268522e3f01");
+//        Sha256Hash blockHash = Sha256Hash.wrap("000203232d");
         Future<Block> future = peer.getBlock(blockHash);
+        
+System.out.println("heaight...."+peer.getBestHeight());
+
+        
+//        peerGroup.downloadBlockChain();
         System.out.println("Waiting for node to send us the requested block: " + blockHash);
         Block block = future.get();
-        System.out.println(block);
-        peerGroup.stopAsync();
+        
+//        System.out.println("Block:- "+block);
+//        System.out.println("Chain heah :"+chain.getChainHead().toString());
+        
+        /*System.out.println("Finding parent block");
+        Sha256Hash PrevBlockHash = block.getPrevBlockHash();
+        Sha256Hash blockHash1 = Sha256Hash.wrap(PrevBlockHash.getBytes());
+        Future<Block> future1 = peer.getBlock(blockHash1);
+        Block blk = future1.get();
+        System.out.println("blk:\n"+blk);
+        System.out.println("Heigh of the block is :"+ blk.getDifficultyTarget());
+        System.out.println("Time :" + blk.getTime());*/
     }
 }
